67d4d777df5138f180b941d5826494789214b7ce
Gerda <gerda.fedotova.sk@lvt.lv>
2024-05-22 12:17:26 +0300

    Scroll View un varona maina darbojas

--- Assets/Script/textScript.cs
+++ Assets/Script/textScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Collections.LowLevel.Unsafe;
@@ -11,12 +12,26 @@ public class textScript : MonoBehaviour
     public GameObject inputField;
     public GameObject inputFieldAge;
     public GameObject displayField;
+    public GameObject heroHolder;
+    public GameObject scroll;
+    public Sprite[] spriteArray;
+
 
     public void getText()
     {
         year = int.Parse(inputFieldAge.GetComponent<Text>().text);
         int age = 2024 - year;
         text = inputField.GetComponent<Text>().text;
-        displayField.GetComponent<Text>().text = "Sveiki, "+text.ToLower()+"! Tev ir "+age+" gadi";
+        displayField.GetComponent<Text>().text = "Sveiki, "+text+"! Tev ir "+age+" gadi";
+    }
+
+    public void changeHero(int index)
+    {
+        heroHolder.GetComponent<Image>().sprite = spriteArray[index];
+        if(index == 0)
+            scroll.GetComponent<Text>().text = "text pro Gerda";
+        else if(index == 1)
+            scroll.GetComponent<Text>().text = "text pro Polina";
+
     }
 }
